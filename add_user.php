f2862ebfff3cb717dea5ae7e2a7db721f91a1109
puneetindersingh <s3558413@w8417905.cs.rmit.edu.au>
2017-04-06 18:52:53 +1000

    Fixes

--- add_user.php
+++ add_user.php
@@ -9,9 +9,14 @@ resetpassword();
 
 function adduser(){
   include 'connect.php';
-
+try{
     $username=$_POST['username'];
       $password=$_POST['password'];
+      if($username==null|| $passoword==null)
+      {
+          echo "<script>alert('Error No data entered(Username or password field cannot be empty)'); window.location = './adminpage.html';</script>";
+          exit();
+      }
       $fname = $_POST['fname'];
      $lname=$_POST['lname'];
      $email = $_POST['email'];
@@ -20,6 +25,15 @@ function adduser(){
       $url=$_POST['url'];
       $jobtitle=$_POST['jobtitle'];
       $admin_status='N';
+
+      $a_status=$_POST['member'];
+      if($a_status!=null)
+      {
+        if($a_status=='admin')
+        {
+          $admin_status='Y';
+        }
+      }
       $sql = "INSERT INTO user_details(username,firstname,lastname,email,phone,companysite,jobtitle,company,password,admin_status)
        VALUES('$username', '$fname', ' $lname', '$email', '$phone', '  $url', '$jobtitle', '$company', '$password', '$admin_status')";
     if ($conn->query($sql) === TRUE) {
@@ -28,6 +42,11 @@ function adduser(){
 } else {
   echo "<script>alert('Error $sql $conn->error'); window.location = './adminpage.html';</script>";
     echo "Error: " . $sql . "<br>" . $conn->error;
+}
+}
+catch(Exception $e)
+{
+  echo $e;
 }}
 
 
@@ -47,7 +66,7 @@ $sql3 = "SELECT * FROM user_details WHERE username='$currentuser' ";
 $result = $conn->query($sql3);
 if($row=$result->fetch_assoc()){
     if($row['password'] == $oldpass){
-      
+
       $sql="UPDATE user_details SET password='$newpass' where username='$currentuser' && password='$oldpass'";
       if ($conn->query($sql) === TRUE) {
 

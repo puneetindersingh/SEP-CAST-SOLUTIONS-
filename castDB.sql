3b41ec7633fa539fe7bb4bc7b2fc48e9b449b2ac
puneetindersingh <s3558413@w8417905.cs.rmit.edu.au>
2017-04-09 16:53:14 +1000

    Db

--- castDB.sql
+++ castDB.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Apr 09, 2017 at 04:02 AM
+-- Generation Time: Apr 09, 2017 at 08:52 AM
 -- Server version: 5.6.35
 -- PHP Version: 7.0.15
 
@@ -19,29 +19,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `castsolution`
 --
-
--- --------------------------------------------------------
-
---
--- Table structure for table `company`
---
-
-CREATE TABLE `company` (
-  `name` varchar(30) NOT NULL,
-  `address` varchar(30) NOT NULL,
-  `phone` varchar(30) NOT NULL,
-  `site` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `company`
---
-
-INSERT INTO `company` (`name`, `address`, `phone`, `site`) VALUES
-('ABC', 'na', '01234565qwewqe', 'ABC.comwe'),
-('ABC2', 'na', '012', 'abc.com'),
-('Microsoft', 'usa', '123456', 'microsift.com'),
-('NA', 'asdaswe', 'weasda', 'asdaswe');
 
 -- --------------------------------------------------------
 
@@ -68,20 +45,14 @@ CREATE TABLE `user_details` (
 --
 
 INSERT INTO `user_details` (`firstname`, `lastname`, `email`, `phone`, `companysite`, `jobtitle`, `company`, `password`, `admin_status`, `username`, `account_status`) VALUES
-('NA', 'NA', 'Na@Na.com', '123456', '', '', '', 'admin', 'Y', 'admin', 'Y'),
-('Amanpreet Singh', ' Gill', 'aman', '123', '  dfvefvfa', 'fvdfs', 'ABC', 'aman', 'N', 'aman', 'N'),
-('Harry', 'Potter', 'asd@gmail.com', '61491234561', 'new', 'boss', 'ABC', 'new', 'N', 'new', 'N'),
-('Pete', 'Singh', 'puneetindersingh@gmail.com', '6112345678910', 'ABC.com', 'NA', 'ABC2', 'user', 'N', 'user', 'N');
+('Amanpreet Singh', 'Gill', 'aman', '123', 'Abc@abc.com', 'fvdfs', 'ABC', 'aman', 'N', 'aman', 'Y'),
+('Pete', 'Singh', 'puneetindersingh@gmail.com', '12345689', '', '', '', 'cast_admin', 'Y', 'cast_admin', 'Y'),
+('Harry', 'Potter', 'asd@gmail.com', '61491234561', 'Abc@abc.com', 'boss', 'ABC', 'harry', 'N', 'harry', 'Y'),
+('Honey', 'Singh', 'honeysingh@gmail.com', '2345678910', 'Abc@abc2.com', 'NA', 'ABC2', 'honey', 'N', 'honey', 'Y');
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `company`
---
-ALTER TABLE `company`
-  ADD PRIMARY KEY (`name`);
 
 --
 -- Indexes for table `user_details`

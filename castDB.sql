02bc929327fead144dfa58fbc7ea6fdc7a6a475e
puneetindersingh <s3558413@w8417905.cs.rmit.edu.au>
2017-04-09 12:03:45 +1000

    New DB pls update @rmit-s3526538-peng-zhang

--- castDB.sql
+++ castDB.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Apr 02, 2017 at 08:05 AM
+-- Generation Time: Apr 09, 2017 at 04:02 AM
 -- Server version: 5.6.35
 -- PHP Version: 7.0.15
 
@@ -38,9 +38,10 @@ CREATE TABLE `company` (
 --
 
 INSERT INTO `company` (`name`, `address`, `phone`, `site`) VALUES
-('ABC', 'na', '0123456', 'ABC.com'),
+('ABC', 'na', '01234565qwewqe', 'ABC.comwe'),
 ('ABC2', 'na', '012', 'abc.com'),
-('asda', 'asdas', 'asda', 'asdas');
+('Microsoft', 'usa', '123456', 'microsift.com'),
+('NA', 'asdaswe', 'weasda', 'asdaswe');
 
 -- --------------------------------------------------------
 
@@ -58,18 +59,19 @@ CREATE TABLE `user_details` (
   `company` varchar(30) NOT NULL,
   `password` varchar(30) NOT NULL,
   `admin_status` varchar(30) NOT NULL,
-  `username` varchar(30) NOT NULL
+  `username` varchar(30) NOT NULL,
+  `account_status` varchar(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `user_details`
 --
 
-INSERT INTO `user_details` (`firstname`, `lastname`, `email`, `phone`, `companysite`, `jobtitle`, `company`, `password`, `admin_status`, `username`) VALUES
-('', '', '', '', '', '', '', 'admin', 'Y', 'admin'),
-('', ' ', '', '', '  ', '', '', 'new', 'N', 'new'),
-('', ' ', '', '', '  ', '', '', 'new user', 'N', 'newuser'),
-('', '', '', '', '', '', '', 'user', 'N', 'user');
+INSERT INTO `user_details` (`firstname`, `lastname`, `email`, `phone`, `companysite`, `jobtitle`, `company`, `password`, `admin_status`, `username`, `account_status`) VALUES
+('NA', 'NA', 'Na@Na.com', '123456', '', '', '', 'admin', 'Y', 'admin', 'Y'),
+('Amanpreet Singh', ' Gill', 'aman', '123', '  dfvefvfa', 'fvdfs', 'ABC', 'aman', 'N', 'aman', 'N'),
+('Harry', 'Potter', 'asd@gmail.com', '61491234561', 'new', 'boss', 'ABC', 'new', 'N', 'new', 'N'),
+('Pete', 'Singh', 'puneetindersingh@gmail.com', '6112345678910', 'ABC.com', 'NA', 'ABC2', 'user', 'N', 'user', 'N');
 
 --
 -- Indexes for dumped tables

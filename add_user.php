2f15a407706b9ce65af053a0bd07f1ad950bd6cc
Frankzp <s3526538@student.rmit.edu.au>
2017-04-09 17:06:33 +1000

    Update add_user.php

--- add_user.php
+++ add_user.php
@@ -17,7 +17,7 @@ try{
      $lname=$_POST['lname'];
      $email = $_POST['email'];
       $phone = $_POST['phone'];
-<<<<<<< HEAD
+
 
 
         $ac_status='Y';
@@ -27,7 +27,7 @@ try{
       $jobtitle="";
       if($a_status!=null)
       {
-=======
+
       $company="";
       $url="";
       $jobtitle="";
@@ -36,7 +36,7 @@ try{
       if(isset($_POST['member'])){
             $a_status=$_POST['member'];
 
->>>>>>> origin/Web-pages
+
         if($a_status=='admin')
         {
           $admin_status='Y';
@@ -57,11 +57,11 @@ try{
        VALUES('$username', '$fname', ' $lname', '$email', '$phone', '  $url', '$jobtitle', '$company', '$password', '$admin_status','$ac_status')";
     if ($conn->query($sql) === TRUE) {
 
-<<<<<<< HEAD
+
     echo "<script>alert('New user added '); window.location = './adminpage.html';</script>";
-=======
+
     echo "<script>alert('New user added'); window.location = './adminpage.html';</script>";
->>>>>>> origin/Web-pages
+
 } else {
   echo "<script>alert('Error $sql $conn->error'); window.location = './adminpage.html';</script>";
     echo "Error: " . $sql . "<br>" . $conn->error;

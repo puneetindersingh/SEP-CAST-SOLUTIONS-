beb1e38bd9792c1e2daca97090c9f853c01dbe02
Frankzp <s3526538@student.rmit.edu.au>
2017-05-01 23:50:51 +1000

    Reschedule the project

---
+++ js/login.js
@@ -0,0 +1,113 @@
+function myFunction1() {
+    var c = document.querySelectorAll(".login");
+    if(c[0].style.display = "block"){
+        c[0].style.display = "none";
+    }
+    var c2 = document.querySelectorAll(".reset");
+    if(c2[0].style.display = "none"){
+        c2[0].style.display = "block";
+    }
+}
+var d = new Date();
+var exdays = 30;
+d.setTime(d.getTime()+(exdays*24*60*60*1000));
+var expires = "expires="+d.toGMTString();
+function setCookie(cname,cvalue,exdays){
+
+	document.cookie = cname+"="+cvalue+"; "+expires;
+}
+
+function getCookie(cname){
+	var name = cname + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0; i<ca.length; i++) {
+		var c = ca[i].trim();
+		if (c.indexOf(name)==0) return c.substring(name.length,c.length);
+	}
+	return "";
+}
+
+function checkCookie(){
+	var user=getCookie("username");
+	if (user!=""){
+		document.getElementById('login-name').value = user;
+	}
+	else {
+
+		user = document.getElementById('login-name').value;
+  		if (user!="" && user!=null){
+    		setCookie("username",user,30);
+    	}
+	}
+}
+
+function setUsername(){
+  setCookie("username",document.getElementById('login-name').value,30);
+}
+
+var hide = document.getElementById('login-pass');
+hide.addEventListener("click",function(){
+  document.getElementById('test').innerHTML = "";
+});
+
+var xmlHttp;
+function showHint(){
+  setUsername();
+  xmlHttp = GetXmlHttpObject();
+if (xmlHttp==null)
+ {
+ alert ("Browser does not support HTTP Request");
+ return 0;
+ }
+ var url='php/function-modified.php';
+ var params = "";
+ params += "login=1&username="+document.getElementById('login-name').value;
+ params += "&password="+document.getElementById('login-pass').value
+ // var params = "username="+document.getElementById('reset-username').value+"&email="+document.getElementById('email-name').value;
+ xmlHttp.open("POST",url,true);
+ xmlHttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+ xmlHttp.onreadystatechange=stateChanged;
+ xmlHttp.send(params);
+}
+
+function stateChanged(){
+if (xmlHttp.readyState==4 || xmlHttp.readyState=="complete"){
+  if(xmlHttp.responseText=="no"){
+    document.cookie = "loginstatus=no; "+expires;
+    document.getElementById('test').innerHTML="<i class='material-icons md-18 red'>warning</i>Username or Password is Wrong,Please try again!";
+    document.getElementById('login-pass').value="";
+  }else if(xmlHttp.responseText=="admin"){
+    document.cookie = "loginstatus=yes; "+expires;
+    window.location = "./adminpage.html";
+  }else if (xmlHttp.responseText=="user") {
+    document.cookie = "loginstatus=yes; "+expires;
+    window.location = "./userpage.html";
+  }else if (xmlHttp.responseText=="not active") {
+    document.getElementById('test').innerHTML="<i class='material-icons md-18 red'>warning</i>Your account is currently suspended please contact support !";
+      document.getElementById('login-pass').value="";
+  }
+ }
+}
+
+function GetXmlHttpObject(){
+var xmlHttp=null;
+try{
+ // Firefox, Opera 8.0+, Safari
+ xmlHttp=new XMLHttpRequest();
+ }
+catch (e){
+ //Internet Explorer
+ try{
+  xmlHttp=new ActiveXObject("Msxml2.XMLHTTP");
+  }
+ catch (e){
+  xmlHttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
+ }
+return xmlHttp;
+}
+
+function popUps() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+}
---
+++ js/userpage.js
@@ -0,0 +1,190 @@
+window.onload=checkCookie();
+function topnav() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "col-12 topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "col-12 topnav";
+    }
+}
+
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+    if(cityName == "modifyProfile"){
+      showProfile();
+    }
+    if(cityName == "qlikPage"){
+      showQlik();
+    }
+}
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+
+function getCookie(cname){
+ var name = cname + "=";
+ var ca = document.cookie.split(';');
+ for(var i=0; i<ca.length; i++) {
+   var c = ca[i].trim();
+   if (c.indexOf(name)==0) return c.substring(name.length,c.length);
+ }
+ return "";
+}
+
+function checkCookie(){
+ var user=getCookie("username");
+  var status=getCookie("loginstatus");
+    var name =getCookie("full_name");
+  if(status!="" && status =="yes"){
+     if (user!=""){
+       document.getElementById('user').innerHTML = name.replace(/\+/g,' ').toUpperCase();
+     }
+     else {
+        alert('no exist Cookie!');
+      }
+    }else{
+      window.location = "./login.html";
+    }
+}
+
+var d = new Date();
+var exdays = 30;
+d.setTime(d.getTime()+(exdays*24*60*60*1000));
+var expires = "expires="+d.toGMTString();
+
+function logOut(){
+  window.location = "./login.html";
+  document.cookie = "loginstatus=no; "+expires;
+}
+
+var xmlHttp;
+function showProfile(){
+  xmlHttp = GetXmlHttpObject();
+if (xmlHttp==null)
+ {
+ alert ("Browser does not support HTTP Request");
+ return 0;
+ }
+ var url='php/function-modified.php';
+ var params = "";
+ params += "profile=1&username="+getCookie('username');
+ // var params = "username="+document.getElementById('reset-username').value+"&email="+document.getElementById('email-name').value;
+ xmlHttp.open("POST",url,true);
+ xmlHttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+ xmlHttp.onreadystatechange=stateChanged;
+ xmlHttp.send(params);
+}
+
+function stateChanged(){
+if (xmlHttp.readyState==4 || xmlHttp.readyState=="complete"){
+  var str=xmlHttp.responseText;
+
+  var obj = eval('(' + str + ')');
+  document.getElementById('firstN').value = obj.firstname;
+  document.getElementById('lastN').value = obj.lastname;
+  document.getElementById('emailAddress').value = obj.email;
+  document.getElementById('phoneNo').value = obj.phone;
+  document.getElementById('companyN').value = obj.company;
+  document.getElementById('companySite').value = obj.companysite;
+  document.getElementById('jobTitle').value = obj.jobtitle;
+ }
+}
+
+function GetXmlHttpObject(){
+var xmlHttp=null;
+try{
+ // Firefox, Opera 8.0+, Safari
+ xmlHttp=new XMLHttpRequest();
+ }
+catch (e){
+ //Internet Explorer
+ try{
+  xmlHttp=new ActiveXObject("Msxml2.XMLHTTP");
+  }
+ catch (e){
+  xmlHttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
+ }
+return xmlHttp;
+}
+
+function updateProfile(){
+  xmlHttp = GetXmlHttpObject();
+if (xmlHttp==null)
+ {
+ alert ("Browser does not support HTTP Request");
+ return 0;
+ }
+ var myreg = /^([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+@([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$/;
+ var myreg1 = /^(\+61)[0-9]{1}[0-9]{4}[0-9]{4}$/;
+ if(!myreg.test(document.getElementById('emailAddress').value)){
+   alert("Please enter the correct email with '@' format!");
+ }else if(!myreg1.test(document.getElementById('phoneNo').value)){
+   alert("Please enter the Phone Number with '+61' at the front and 9 following numbers!");
+ }else{
+ var url='php/function-modified.php';
+ var params = "";
+ var phone = document.getElementById('phoneNo').value;
+ phone = phone.replace(/\+/g, "%2B");
+ params += "updateprofile=1&username="+document.getElementById('user').innerHTML+"&firstname="+document.getElementById('firstN').value
+ +"&lastname="+document.getElementById('lastN').value+"&email="+document.getElementById('emailAddress').value
+ +"&phone="+phone+"&companyname="+document.getElementById('companyN').value
+ +"&companysite="+document.getElementById('companySite').value+"&jobtitle="+document.getElementById('jobTitle').value;
+
+ // var params = "username="+document.getElementById('reset-username').value+"&email="+document.getElementById('email-name').value;
+ xmlHttp.open("POST",url,true);
+ xmlHttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  xmlHttp.onreadystatechange=stateChangedforProfie;
+ xmlHttp.send(params);
+}
+}
+
+function stateChangedforProfie(){
+if (xmlHttp.readyState==4 || xmlHttp.readyState=="complete"){
+  alert(xmlHttp.responseText);
+ }
+}
+
+function showQlik(){
+  xmlHttp = GetXmlHttpObject();
+if (xmlHttp==null)
+ {
+ alert ("Browser does not support HTTP Request");
+ return 0;
+ }
+ var url='php/function-modified.php';
+ var params = "";
+ params += "qlik=1&username="+getCookie('username');
+ // var params = "username="+document.getElementById('reset-username').value+"&email="+document.getElementById('email-name').value;
+ xmlHttp.open("POST",url,true);
+ xmlHttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+ xmlHttp.onreadystatechange=stateChangedforQlik;
+ xmlHttp.send(params);
+}
+
+function stateChangedforQlik(){
+if (xmlHttp.readyState==4 || xmlHttp.readyState=="complete"){
+  var str=xmlHttp.responseText;
+  var obj = eval('(' + str + ')');
+  var arr = new Array(obj.iframe1,obj.iframe2,obj.iframe3,obj.iframe4,obj.iframe5,obj.iframe6);
+  var mess="<h4>Qlik Sense</h4><div class='rowTeam'>";
+  mess += "<div class='col-12 columnQlik'><div class='helpCard'><iframe style='border:none;height:140px;width:100%;' src='https://analytics.castsolutions.com.au/single/?appid=223c0fbe-8420-4344-bbd9-f4227eb2a3a3&obj=DTpyBG&opt=nointeraction&select=clearall' ></iframe></div></div>";
+  for(var a=0;a<arr.length;a++){
+    if(arr[a] != ""){
+      mess += "<div class='col-6 columnQlik'><div class='helpCard'><iframe style='border:none;height:500px;width:100%;' src='"+arr[a]+"' ></iframe></div></div>";
+    }
+  }
+  mess +="</div>";
+  document.getElementById('qlikPage').innerHTML = mess;
+ }
+}
+// <div class='col-6 columnQlik'><div class='helpCard'><iframe style='border:none;height:500px;width:100%;' src='"+arr[0]+"' ></iframe></div></div>

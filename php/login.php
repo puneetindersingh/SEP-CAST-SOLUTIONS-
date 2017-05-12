a90f699121176df9d47af819f982ee84696f63d7
Kyle-Li-RMIT <s3502840@student.rmit.edu.au>
2017-05-12 16:17:34 +1000

    delete mail password

--- php/login.php
+++ php/login.php
@@ -58,7 +58,7 @@ if($check==1){
        $mail->Host = 'smtp.gmail.com';                    // Specify main and backup SMTP servers
        $mail->SMTPAuth = true;                            // Enable SMTP authentication
        $mail->Username = 'pengzhang1925@gmail.com';          // SMTP username
-       $mail->Password = 'QQ0819xy'; // SMTP password
+       $mail->Password = ''; // SMTP password
        $mail->SMTPSecure = 'tls';                         // Enable TLS encryption, `ssl` also accepted
        $mail->Port = 587;// TCP port to connect to
       //  $mail->SMTPDebug  = 4;       //For debug the server connection
